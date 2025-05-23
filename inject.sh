09741e7a403ec8d96e9e324f3b1f3c0df613367c
Walag <bannansmooth68@outlook.com>
2025-05-23 17:06:08 -0400

    add inject.sh

---
+++ inject.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script is still under construction :)
+
+echo "This simple script injects any text you want into any file!"
+
+sleep 2
+
+echo "Type the path to the file that you want to inject into"
+read injectpath
+
+if [ ! -f $injectpath ]; then
+	echo "File does not exist!"
+	exit 1
+fi
+
+sleep 1
+
+echo "Type the text you want to inject"
+read inject
+
+echo "Injecting ..."
+sleep 1.5
+echo $inject >> $injectpath
+echo "Done!"
