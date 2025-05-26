2a72ba024403e13aa878e943d429b7f0dd6b59de
Walag <bannansmooth68@outlook.com>
2025-05-26 17:15:14 -0400

    update

--- kali.sh
+++ kali.sh
@@ -3,19 +3,19 @@
 # Changes the apt sources
 echo 'deb http://kali.download/kali kali-rolling main non-free contrib' > /etc/apt/sources.list
 
+echo 'deb http://http.kali.org/kali kali-rolling main non-free-firmware non-free contrib' > /etc/apt/sources.list
+
+
 # Obtain the apt key
 wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc
-
-# Installs kali key package
-wget -q --show-progress https://kali.download/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2025.1_all.deb
-
-dpkg -i kali-archive-keyring_2025.1_all.deb
 
 # Update
 sudo apt update
 sudo apt upgrade -y
 sudo apt install kali-defaults -y
+
 sudo apt update
 sudo apt upgrade -y
-sudo apt autoremove
+sudo apt install firmware-mediatek
+
 
